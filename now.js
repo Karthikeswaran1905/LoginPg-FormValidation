4528961fde1ca28abe5a31615d4307fcaf373233
Karthikeswaran.V <137477250+Karthikeswaran1905@users.noreply.github.com>
2023-07-24 21:18:05 +0530

    Add files via upload

---
+++ now.js
@@ -0,0 +1,16 @@
+alert("You Are Inside a Stranger website");
+
+function whole() {
+   var user = document.getElementById("us");
+   var password = document.getElementById("pass");
+  
+   if(user.value =="" || password.value =="")
+   {
+    alert("You Have Not Given The Right Information Check It Again!!! ");
+    return false;
+    }else{
+      alert("Going To The Official Website")
+    true;
+    }
+
+}
